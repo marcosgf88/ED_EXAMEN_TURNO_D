74961b7cf8d51a618693009d9ae8f83ac8fb0ef2
Marcos_gf <marcosgf88@gmail.com>
2021-01-14 13:02:54 +0100

    commit nuevo proyecto java

---
+++ AnalistaED_48410434/src/AnalistaED_48410434.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+
+public class AnalistaED_48410434 {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public AnalistaED_48410434(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+
+    // Este método no lo comentaremos con JavaDoc
+    public static void main(String[] args) {
+        // !!!! MODIFICA TU NOMBRE Y TU DNI EN LA SIGUIENTE LÍNEA        
+        AnalistaED_48410434 emp = new AnalistaED_48410434("SERGIO BADAL", "44654545X");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto 
+		+ " este mes será de " + emp.getSalarioEsteMes(horasExtra)
+                + " euros, al hacer " + horasExtra + " horas extra.");
+    }
+
+}
